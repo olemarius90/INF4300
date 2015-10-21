2301b2f0eb9905dd64897bcb02010ff21bd64125
Ole Marius Hoel Rindal <olemarius@olemarius.net>
2015-10-21 11:26:46 +0200

    "Added solution to exercise week 7 "

--- Week7/exercise.m
+++ Week7/exercise.m
@@ -1,124 +1,147 @@
+%% Univariate Gaussian Classifier
+% A suggested "solution" to classification exercise 1 the univariate
+% Gaussian classifier. This week I will not provide the actual solution, it
+% is hidden in the UnivariateGauss function. Since it is a point that you
+% should implement this yourself. Anyway, I have provided some results so
+% that you have something to compare your code with. If you have large
+% differences from my result, please let me know and we'll check it out.
 
 clear all
 close all
+%% The feature images
+% We are provided with a Landsat sattelite image containing 6 image
+% "bands". We will use each of these bands as a feature image. Lets load
+% them, I'm saving them in a so called cell array.
+tm = cell(6);
 tm{1} = imread('tm1.png');
 tm{2} = imread('tm2.png');
 tm{3} = imread('tm3.png');
 tm{4} = imread('tm4.png');
 tm{5} = imread('tm5.png');
 tm{6} = imread('tm6.png');
+% We are also going to need these training and test masks. These masks
+% indicate what pixels are known to belong to each class. Thus indicating
+% what pixels we should use for training, and what pixels we can use to
+% validate the result (test).
 tm_train = imread('tm_train.png');
 tm_test = imread('tm_test.png');
 
 figure(1)
-subplot(421)
-imshow(tm_train,[]);
-colorbar
-title('Training mask');
-
-subplot(422)
-imshow(tm_test,[]);
-colorbar
-title('Test mask');
-
-subplot(423)
+subplot(211)
 imshow(tm{1},[]);
 colorbar
 title('Feature 1');
 
-subplot(424)
+subplot(212)
 imshow(tm{2},[]);
 colorbar
 title('Feature 2');
 
-subplot(425)
+figure(2)
+subplot(211)
 imshow(tm{3},[]);
 colorbar
 title('Feature 3');
 
-subplot(426)
+subplot(212)
 imshow(tm{4},[]);
 colorbar
 title('Feature 4');
 
-subplot(427)
+figure(3)
+subplot(211)
 imshow(tm{5},[]);
 colorbar
 title('Feature 5');
 
-
-subplot(428)
+subplot(212)
 imshow(tm{6},[]);
 colorbar
 title('Feature 6');
-%%
 
-[N,M] = size(tm{1});
-k = 4;                      %Number of classes
-number_of_features = 6;     %Number of features
-apriori = 1/k;              %A priori probability
-class = zeros(N,M);         %Define variables
-confidence = zeros(1,k);
-u = zeros(1,k);
-v = zeros(1,k);
-nbr_classified = sum(tm_test(:)>0);
-for feature = 1%:number_of_features
-    figure
+figure(4)
+subplot(211)
+imagesc(tm_train);
+colorbar
+axis image
+title('Training mask');
 
-    %For every class
-    for i = 1:k
-        %Estimate the mean and variance
-        u(i) = mean(tm{feature}(tm_train==i));
-        v(i) = var(double(tm{feature}(tm_train==i)));
-        
-        %Plot the resuting Gaussian distributions
-        x = linspace(-100,100,1000);
-        p = apriori*(1/(sqrt(2*pi)*sqrt(v(i))))*exp(-(x-u(i)).^2/(2*v(i)));
-        subplot(221)
-        hold all
-        plot(x,p)
-        legend_txt{i} = ['Class ',num2str(i)];
-        title(['Feature ',num2str(feature)]);
-    end
-    legend(legend_txt)
+subplot(212)
+imagesc(tm_test);
+colorbar
+axis image
+title('Test mask');
+drawnow
+%% Classification
+% We are going to use the univariate gaussian classifier, meaning that we
+% are only unsing one feature at a time and classify the image with just
+% one feature. The goal of this exercise is to evaluate each feature and
+% figure out which seems to be the best.
+number_of_features = size(tm,2);     %Number of features
+k = double(max(tm_train(:)));        %Number of classes
+nbr_classified = sum(tm_test(:)>0);  %The total number of pixels in the test set
+[N,M] = size(tm{1});                 %Size of image
 
-    %Classify every pixel based on what
-    %class has the highest probability
-    for i = 1:N
-        for t = 1:M
-            %Calculate probability for every class
-            for j = 1:k
-                confidence(j) = apriori*(1/(sqrt(2*pi)*sqrt(v(j))))*exp(-(double(tm{feature}(i,t))-u(j)).^2/(2*v(j)));
-            end
-            %Choose the class with the highest probability
-            [c,class(i,t)] = max(confidence);
-        end
-    end
+for feature = 1:number_of_features
+    
+    %We are sending the images into this "black box" that you should
+    %implement and we are getting the resulting class.
+    fig_nbr = 5+feature;
+    class = UnivariateGauss(feature,tm_train,tm,fig_nbr);
     
     %Make a labeled image to display how the pixels were classified
-    subplot(222)
-    imshow(class,[])
-    title(['Feature ',num2str(feature)]);
+    figure(100+feature);clf
+    imagesc(class)
+    colormap jet
+    colorbar
+    title(['Feature: ',num2str(feature),', classification result']);
+    drawnow
+    
+    %Lest mask out the traning part of the image
+    img_labeled_train = zeros(N,M);
+    img_labeled_train(tm_train==1) = class(tm_train==1);
+    img_labeled_train(tm_train==2) = class(tm_train==2);
+    img_labeled_train(tm_train==3) = class(tm_train==3);
+    img_labeled_train(tm_train==4) = class(tm_train==4);
+    
+    figure(200+feature);
+    imagesc(img_labeled_train);
+    colormap jet
+    colorbar 
+    title(['Feature: ',num2str(feature),', masked result (train) for the classes']);
+    drawnow
+    
+    %And the test part of the image
     img_labeled = zeros(N,M);
     img_labeled(tm_test==1) = class(tm_test==1);
-    %img_labeled(tm_test==2) = class(tm_test==2);
-    %img_labeled(tm_test==3) = class(tm_test==3);
-    %img_labeled(tm_test==4) = class(tm_test==4);
-    subplot(223)
-    imshow(img_labeled,[])
+    img_labeled(tm_test==2) = class(tm_test==2);
+    img_labeled(tm_test==3) = class(tm_test==3);
+    img_labeled(tm_test==4) = class(tm_test==4);
+    
+    figure(300+feature);
+    imagesc(img_labeled)
+    colormap jet
+    colorbar
+    title(['Feature: ',num2str(feature),', masked result (test) for the classes']);
+    drawnow
     
     %Calculate the correct classification
     fprintf('Feature %d: \n',feature);
     correct = zeros(1,k);
     percent = zeros(1,k);
     error = 0;
+    %Lets go through each class and calculate
     for class_index = 1:k
+        % The error: by summing up how many class labels does not
+        % correspond with the class index.
         error = error + sum(class(tm_test==class_index)~=class_index);
+        % The correct is thus how many does fit
         correct(class_index) = sum(class(tm_test==class_index)==class_index);
         percent = correct(class_index)/sum(sum(tm_test==class_index));
         fprintf('Class %d correct: %f \n',class_index,percent*100);
     end
-    fprintf('Error: %f\n',error/nbr_classified);
-    fprintf('Correct classification for feature image %d is %f \n\n\n',feature,sum(correct)*100/nbr_classified);
+    fprintf('Total error: %f\n',error/nbr_classified);
+    fprintf('Correct classification for feature image %d is %f \n\n\n',...
+        feature,sum(correct)*100/nbr_classified);
 
 end
